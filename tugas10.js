6e61e2d543bddbda4349753ae1189af4bd1d74aa
maitasawilia <maitasaw@gmail.com>
2020-06-23 22:55:46 +0700

    Tugas 10 JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas10.js
@@ -0,0 +1,18 @@
+var a = 2;
+var b = 1;
+var c = 3;
+var d = 5;
+// var e = 7;
+// var f = 4;
+// var g = 6;
+
+console.log(a-b)
+console.log(a+b)
+console.log(a+b+c)
+console.log(a*d)
+console.log(c*d)
+console.log((a+d)*c)
+console.log((a+d)*(d-b))
+console.log((b+d)*(b+d))
+console.log((b+c+d)*d)
+console.log((d+d+b)*d)
